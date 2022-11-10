c18a8337f2a7584a818ae2ba19e3bafdd579b4db
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2022-11-10 11:05:07 +0000

    Add input and output file selection

---
+++ src/main/java/io/github/shaksternano/pdfcreator/Main.java
@@ -0,0 +1,55 @@
+package io.github.shaksternano.pdfcreator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        File input = new File("");
+        while (!input.isFile()) {
+            System.out.print("Enter the path to the input file: ");
+            String inputPath = scanner.nextLine();
+            input = new File(inputPath);
+            if (!input.isFile()) {
+                System.out.println("The file does not exist!");
+            }
+        }
+
+        File outputDir = new File("");
+        while (!outputDir.isDirectory()) {
+            System.out.print("Enter the path to the output directory. Leave empty for the input file directory: ");
+            String outputPath = scanner.nextLine();
+            if (outputPath.isBlank()) {
+                outputDir = input.getAbsoluteFile().getParentFile();
+            } else {
+                outputDir = new File(outputPath);
+                if (outputDir.isFile()) {
+                    System.out.println("There is already a file with the same name!");
+                } else {
+                    outputDir.mkdirs();
+                }
+            }
+        }
+
+        System.out.print("Enter the PDF name: ");
+        String pdfName = scanner.nextLine();
+        if (!pdfName.toLowerCase().endsWith(".pdf")) {
+            pdfName += ".pdf";
+        }
+        File output = new File(outputDir, pdfName);
+        PDFCreator pdfCreator = new PDFCreator(
+                input,
+                output,
+                PDFFont.HELVETICA,
+                50,
+                50,
+                1.25F
+        );
+        pdfCreator.create();
+        System.out.println("PDF created at: " + output.getAbsolutePath());
+    }
+}
